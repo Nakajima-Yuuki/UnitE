565488919a833c21d5975ed7186cf3dc1ba66115
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-09-30 17:43:14 +0900

    TOPページ作成

--- app/controllers/proposers/registrations_controller.rb
+++ app/controllers/proposers/registrations_controller.rb
@@ -53,12 +53,12 @@ class Proposers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
+  def after_sign_up_path_for(resource)
     proposer_path(id: current_proposer.id)
   end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
