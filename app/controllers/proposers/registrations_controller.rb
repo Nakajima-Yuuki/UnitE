84d8274dbd474000092a0d9cfb2153522e97064c
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-09-30 16:22:21 +0900

    ログイン画面のviewを整える

--- app/controllers/proposers/registrations_controller.rb
+++ app/controllers/proposers/registrations_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Proposers::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  
+  before_action :authenticate_proposer!
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,12 +40,12 @@ class Proposers::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -56,7 +58,7 @@ class Proposers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    proposer_path(id: current_proposer.id)
+  end
 end
