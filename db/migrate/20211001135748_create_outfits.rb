6dca1557b7fbfa2113775e5f1063c5a20cca1298
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-10-01 23:10:09 +0900

    outfitテーブル作成ン

--- db/migrate/20211001135748_create_outfits.rb
+++ db/migrate/20211001135748_create_outfits.rb
@@ -1,8 +1,9 @@
 class CreateOutfits < ActiveRecord::Migration[5.2]
   def change
     create_table :outfits do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :content
+      t.references :proposer, foreign_key: true, null: false
 
       t.timestamps
     end
