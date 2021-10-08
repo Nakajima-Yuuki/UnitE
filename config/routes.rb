275818af385b1c357ac6dfcedb7b8d77fe97f7bf
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-10-08 10:55:18 +0900

    railsadmin導入 exit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :outfits
   resources :stocks, only: [:create, :destroy]
   post 'like/:id' => 'likes#create', as: 'create_like'
