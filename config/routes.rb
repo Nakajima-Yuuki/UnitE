fb6338d488a503092e2464477336c6375bed61c6
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-10-09 11:15:21 +0900

    ユーザーゲストログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users, only: [:show]
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
 
