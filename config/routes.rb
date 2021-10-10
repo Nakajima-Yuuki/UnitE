4d9e878c47d6cd6b853489c6616e61db216dc662
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-10-10 17:06:42 +0900

    プロポーサーゲストログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
+  devise_scope :proposer do
+    post 'proposers/guest_sign_in', to: 'proposers/sessions#guest_sign_in'
+  end
+
 end
 
