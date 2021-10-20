9ed724cfe07b9b25c43ce20dc60245fd8157dd9b
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-10-21 00:49:58 +0900

    バリデーション変更

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -7,18 +7,4 @@ class Outfit < ApplicationRecord
     has_many :stocks, dependent: :destroy
     has_many :stocks_users, through: :stocks, source: :user
     has_many :likes, dependent: :destroy
-  # 現在ログインしているユーザーidを受け取り、記事をストックする
-  def stock(user)
-    stocks.create(user_id: user.id)
-  end
-  
-  # 現在ログインしているユーザーidを受け取り、記事のストックを解除する
-  def unstock(user)
-    stocks.find_by(user_id: user.id).destroy
-  end
-  
-  # 記事がストック済みであるかを判定、取得済みであれば true を返す
-  def stocked?(user)
-    stock_users.include?(user)
-  end
 end
