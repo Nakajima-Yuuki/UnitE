b2343aa572188bbdf238f5328fe630fdcdc835c6
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-10-21 11:33:31 +0900

    要件修正

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,18 @@
 class StocksController < ApplicationController
   before_action :authenticate_user!
   
+  
   def create
-    #if @outfit.stocked?(current_user)
+    #@outfit = Outfit.find(params[:outfit_id])
+    #unless @outfit.stocks(current_user)
       stock = current_user.stocks.create(outfit_id: params[:outfit_id])
       redirect_to outfit_path(params[:outfit_id]), notice: "#{stock.outfit.proposer.name}さんのブログをお気に入り登録しました"
     #end
   end
   
   def destroy
-    #if @outfit.stocked?(current_user)
+    #@outfit = Outfit.find(params[:id]).outfit
+    #if @outfit.stocks(current_user)
       stock = current_user.stocks.find_by(id: params[:id]).destroy
       redirect_to outfit_path(stock.outfit_id), notice: "#{stock.outfit.proposer.name}さんのブログをお気に入り解除しました"
     #end
